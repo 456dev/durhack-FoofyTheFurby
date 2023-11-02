511e8f56916a605e75cbec56db7592f6a742b966
The456gamer <the456gamer@the456gamer.dev>
2023-11-02 20:28:37 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+# on file change
+
+# openai api with updated code?
+
+# parse json response
+
+# get furby action
+
+
+
+# code input
+# processing
+# output
+
+
+# output supports:
+# read furby hardware
+# furby emulator
