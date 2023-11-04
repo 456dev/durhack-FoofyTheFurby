2e740d6a8067cced8639c3a260b783f5b15fa505
The456gamer <the456gamer@the456gamer.dev>
2023-11-04 22:09:00 +0000

    librosa
    Co-authored-by: Winkerwatty <Winkerwatty@users.noreply.github.com>

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import time
 import pathlib
 import pydub, pydub.playback
 import random
+import librosa
 
 from typing import Sequence
 
@@ -111,11 +112,14 @@ def play_and_modulate(comment_frequency: int, style: int, error_guess: int,
   comment_politeness: int, voice_name: str, force:bool = False):
     path = ensure_file_generated(comment_frequency, style, error_guess, comment_politeness, voice_name, force)
 
+    y, sr = librosa.load(path)
+    librosa.effects.pitch_shift(y, sr=sr, )
+
     audioseg = pydub.AudioSegment.from_wav(path)
     subsegments = audioseg[::100]
-    subsegments = [(seg + random.randrange(-20, 50)/10) for seg in subsegments]
-    full_modulated = sum(subsegments)
-    pydub.playback.play(audioseg)
+    new_subsegments = [(seg + random.randrange(-60, 20)/10) for seg in subsegments]
+    full_modulated = sum(new_subsegments)
+    pydub.playback.play(full_modulated)
 
 
 play_and_modulate(0, 100, 100, 100, "en-GB-Neural2-A", True)
