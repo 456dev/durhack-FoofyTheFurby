454240ef64b2b733af4b46809f1b8c060a194e5a
The456gamer <the456gamer@the456gamer.dev>
2023-11-04 19:55:45 +0000

    update

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 import argparse
 import os
 import pprint
+import sys
 
 import openai
 import dotenv
 import json
 import pydantic
 import time
+import pathlib
 
 from typing import Sequence
 
 import google.cloud.texttospeech as tts
 
 dotenv.load_dotenv()
+
+parser = argparse.ArgumentParser(
+                    prog='FurbyReviewer',
+                    description='Forces the Furby to watch you code.',
+                    epilog='God help us all.')
+
+parser.add_argument("-f", "--filename", default="code.txt")
+args = parser.parse_args()
+
+def get_voiceline_string(comment_frequency: int, style: int, error_guess: int, comment_politeness: int):
+  abnormal_code = all([abs(factor - 100) <= 25 for factor in [comment_frequency, style, error_guess, comment_politeness]])
+  comment_alignment = 0 if abs(comment_politeness - 100) <= 25 else (-1 if comment_politeness < 100 else 1)
+  comment_alignment_is_positive = comment_alignment == 0
+  comment_frequency_is_positive = abs(comment_frequency - 100) <= 25
+  comment_matches = comment_alignment_is_positive == comment_frequency_is_positive
+
+  f"""
+  <speak>
+    This code is {"<break time=\"1s\"/>interesting" if abnormal_code else "acceptable"}.
+    Your comments are {"creepy" if comment_alignment == -1 else ("bad" if comment_alignment == -1 else "good")}, {"and" if comment_matches else "but"} there are {"just" if comment_frequency_is_positive "not"} enough of them.
+  </speak>
+  """
 
 
 def unique_languages_from_voices(voices: Sequence[tts.Voice]):
@@ -47,7 +71,7 @@ def list_voices(language_code=None):
         print(f"{languages:<8} | {name:<24} | {gender:<8} | {rate:,} Hz")
 
 
-def text_to_wav(voice_name: str, text: str):
+def text_to_wav(voice_name: str, text: str, file: pathlib.Path):
     language_code = "-".join(voice_name.split("-")[:2])
     text_input = tts.SynthesisInput(text=text)
     voice_params = tts.VoiceSelectionParams(
@@ -61,11 +85,22 @@ def text_to_wav(voice_name: str, text: str):
         voice=voice_params,
         audio_config=audio_config,
     )
-
-    filename = f"{voice_name}.wav"
-    with open(filename, "wb") as out:
+    with open(file, "wb") as out:
         out.write(response.audio_content)
-        print(f'Generated speech saved to "{filename}"')
+        print(f'Generated speech saved to "{file.name}"')
+
+
+    def a(comment_frequency: int, style: int, error_guess: int, comment_politeness: int, voice_name:str):
+        file_name = f"cf{comment_frequency}_st{style}_eg{error_guess}_cn{comment_politeness}_vc{voice_name}.wav"
+        path = pathlib.Path("media/"+file_name)
+
+        text = get_voiceline_string(comment_frequency, style, error_guess, comment_politeness)
+
+        if not path.exists():
+            text_to_wav(voice_name, text, path)
+
+
+
 
 
 list_voices("en")
@@ -101,6 +136,7 @@ custom sounds
 def on_error(msg: str):
     print("furby.babble_nonsense()")
     print(f"[!] Error: {msg}")
+
     raise NotImplementedError(msg)
 
 
@@ -185,20 +221,8 @@ assistant:
 }
 """
 
-
-input_filename = ""
-
-parser = argparse.ArgumentParser(
-                    prog='FurbyReviewer',
-                    description='Forces the Furby to watch you code.',
-                    epilog='God help us all.')
-
-parser.add_argument("-f", "--filename", default="code.txt")
-args = parser.parse_args()
-
-# TODO change to cli input
 print("[>] Furby.Thinking(1)")
-print("[*] loading File")
+print(f"[*] loading File {args.filename}")
 with open(args.filename, "r") as file:
     x = file.readlines()
     print("[*] Read Lines")
@@ -219,6 +243,8 @@ try:
       )
 except openai.error.Timeout as e:
     on_error("OpenAi TIMEOUT")
+except openai.error.ServiceUnavailableError as e:
+    on_error("OpenAi Unavalible")
 
 print(f"[*] Got Response in {time.time() - time_to_ai}")
 response = completion['choices'][0]['message']['content']
@@ -249,7 +275,7 @@ print(ai_response.comment_politeness_reason)
 print(f"[I] Score of {ai_response.comment_politeness}")
 Upperbound = 75
 Lowerbound = 25
-if Lowerbound < (ai_response.comment_politeness / 2) < Upperbound:  # sees if comments are bad mannered or creepy nice
+if 2*Lowerbound < ai_response.comment_politeness < 2 * Upperbound:  # sees if comments are bad mannered or creepy nice
     lowestValue = 100
     highestValue = 0
     for x in Scores:  # loops to find highest and lowest score
@@ -269,16 +295,3 @@ else:
 print("[>] Furby.Idle=true")
 
 
-# on file change
-
-# openai api with updated code?
-
-# parse json response
-
-# get furby action
-
-
-# input:
-# file (once per run)
-# file (run on change)
-# api
