2d5fefca99efd897231151bee70a8e530bc0b2ac
jellybeanstaffy <40145818+jellybeanstaffy@users.noreply.github.com>
2023-11-05 10:32:03 +0000

    Add files via upload

--- furby2.py
+++ furby2.py
@@ -3,7 +3,7 @@ import time
 
 ghost = False
 
-arduino = serial.Serial(port='/dev/ttyACM1',  baudrate=9600, timeout=.1)
+arduino = serial.Serial(port='/dev/ttyACM0',  baudrate=9600, timeout=.1)
 
 def init():
     arduino.write("home".encode())
@@ -31,7 +31,8 @@ def wake():
 def boogie():
     arduino.write("home".encode())
     while str(arduino.readline().decode().rstrip()) != "home return":
-        time.sleep(0.05)    arduino.write("boogie".encode())
+        time.sleep(0.05)
+        arduino.write("boogie".encode())
     while str(arduino.readline().decode().rstrip()) != "boogie return":
         time.sleep(0.05)
     print("furby boogie")
@@ -55,7 +56,7 @@ def startyapping():
     arduino.write("speak".encode())
 
 def stopyapping():
-    arduino.write("nospeak".encode())
     time.sleep(0.1)
+    arduino.write("nospeak".encode())
     while str(arduino.readline().decode().rstrip()) != "home return":
         time.sleep(0.05)
